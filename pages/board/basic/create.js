0215aa6c8080fea1f7253f5a3bf3ced8059ff09e
jt01052552295 <jt01052552295@gmail.com>
2022-09-26 17:34:22 +0900

    페이지버그수정

---
+++ pages/board/basic/create.js
@@ -0,0 +1,32 @@
+import AppLayout from '../../../components/templates/AppLayout';
+import {
+  useState,
+  useEffect,
+  useRouter,
+  FontAwesomeIcon,
+  Link,
+  Image,
+  axios,
+} from '../../../libraries';
+import { faSearch, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+export default function Create() {
+  return (
+    <div>
+      <AppLayout
+        mode={'dev'}
+        title="404"
+        description=""
+        showLeftSidebar={false}
+        showRightSidebar={false}
+        showHeader={false}
+        showFooter={true}
+      >
+        <div className="d-flex align-items-center justify-content-center vh-100">
+          <div className="text-center">
+            <h1 className="display-1 fw-bold">Create</h1>
+          </div>
+        </div>
+      </AppLayout>
+    </div>
+  );
+}
