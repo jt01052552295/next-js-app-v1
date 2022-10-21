040c8c7e1b864e3356973d915142119bec1da2ef
jt01052552295 <jt01052552295@gmail.com>
2022-10-21 15:24:28 +0900

    서브페이지추가

--- components/organisms/SiteFooter.js
+++ components/organisms/SiteFooter.js
@@ -1,13 +1,26 @@
-import { React, Link, PropTypes, Fragment, useRouter } from '/libraries';
+import { React, Link, PropTypes, Fragment, useRouter, FontAwesomeIcon } from '/libraries';
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
 
 export default function SiteFooter({ ...props }) {
   // console.log(props);
+
+  const goToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
 
   return (
     <div className="container">
       <footer className="justify-content-center py-3 my-4 border-top">
         <p className="text-center text-muted">&copy; 2022 Company, Inc</p>
       </footer>
+      <div className="top-to-btm">
+        <button type="button" className="btn btn-dark" onClick={goToTop}>
+          <FontAwesomeIcon icon={faArrowUp} />
+        </button>
+      </div>
     </div>
   );
 }
--- pages/pricing.js
+++ pages/pricing.js
@@ -17,7 +17,7 @@ export default function About() {
       >
         <div className="d-flex align-items-center justify-content-center vh-100">
           <div className="text-center">
-            <h1 className="display-1 fw-bold">About</h1>
+            <h1 className="display-1 fw-bold">Pricing</h1>
           </div>
         </div>
       </AppLayout>
