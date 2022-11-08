25979de1f3df8614a211f5fa23d3dc44f8ca3ab0
jt01052552295 <jt01052552295@gmail.com>
2022-11-08 12:17:01 +0900

    try react-query

---
+++ pages/example.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import AppLayout from '../components/templates/AppLayout';
+import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+export default function Example() {
+  const { isLoading, error, data, isFetching } = useQuery(['repoData'], () =>
+    axios.get('https://api.github.com/repos/tannerlinsley/react-query').then((res) => res.data),
+  );
+
+  if (isLoading) return 'Loading...';
+
+  if (error) return 'An error has occurred: ' + error.message;
+
+  return (
+    <div>
+      <AppLayout
+        mode={'dev'}
+        title="404"
+        description=""
+        showLeftSidebar={false}
+        showRightSidebar={false}
+        showHeader={false}
+        showFooter={false}
+      >
+        <div className="d-flex align-items-center justify-content-center vh-100">
+          <div className="text-center">
+            <h1 className="display-1 fw-bold">Example</h1>
+            <p>{data.description}</p>
+            <strong>👀 {data.subscribers_count}</strong> <strong>✨ {data.stargazers_count}</strong>{' '}
+            <strong>🍴 {data.forks_count}</strong>
+            <div>{isFetching ? 'Updating...' : ''}</div>
+          </div>
+        </div>
+      </AppLayout>
+    </div>
+  );
+}
